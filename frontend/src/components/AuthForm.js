55603d04a55faa46c6de0685e189665808fee4e3
HS <mcm009@naver.com>
2025-03-22 15:02:31 +0900

    test : 테스트용 더미 로그인 데이터 주석처리, 기존 로그인 로직으로 변경

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -19,26 +19,26 @@ const AuthForm = ({ onLogin }) => {
 
     //풀어야 하는 주석함수
 
-    //   try {
-    //     if (mode === 'login') {
-    //       const response = await axios.post('http://localhost:5000/users/login', { loginId: loginId, pwd: pwd });
-    //       console.log(response)
-    //       if (response.status === 200) {
-    //         localStorage.setItem('userId', JSON.stringify(response.data.id));
-    //         onLogin(response.data.id); 
-    //       }
-    //     } else {
-    //       const response = await axios.post('http://localhost:5000/users/join', { loginId: loginId, pwd: pwd });
-    //       if (response.status === 201) {
-    //         alert('회원가입이 완료되었습니다. 로그인 해주세요.');
-    //         setMode('login'); // 로그인 화면으로 전환
-    //         resetForm(); // 폼 초기화
-    //       }
-    //     }
-    //   } catch (error) {
-    //     setErrorMessage(error.response.data.message);
-    //   }
-    // };
+      try {
+        if (mode === 'login') {
+          const response = await axios.post('http://localhost:5000/users/login', { loginId: loginId, pwd: pwd });
+          console.log(response)
+          if (response.status === 200) {
+            localStorage.setItem('userId', JSON.stringify(response.data.id));
+            onLogin(response.data.id); 
+          }
+        } else {
+          const response = await axios.post('http://localhost:5000/users/join', { loginId: loginId, pwd: pwd });
+          if (response.status === 201) {
+            alert('회원가입이 완료되었습니다. 로그인 해주세요.');
+            setMode('login'); // 로그인 화면으로 전환
+            resetForm(); // 폼 초기화
+          }
+        }
+      } catch (error) {
+        setErrorMessage(error.response.data.message);
+      }
+    };
 
     if (mode === 'register' && pwd !== confirmPassword) {
       setErrorMessage('비밀번호가 일치하지 않습니다.');
@@ -46,21 +46,21 @@ const AuthForm = ({ onLogin }) => {
     }
 
     // 테스트를 위한 하드코딩 하단 코드를 주석 처리하고 상단 코드를 풀면 정상작동!
-    if (mode === 'login') {
-      // 하드코딩된 사용자 정보로 로그인 처리
-      const dummyUserId = 'dummyUser';
-      localStorage.setItem('userId', JSON.stringify(dummyUserId));
-      onLogin(dummyUserId);
-    } else {
+  //   if (mode === 'login') {
+  //     // 하드코딩된 사용자 정보로 로그인 처리
+  //     const dummyUserId = 'dummyUser';
+  //     localStorage.setItem('userId', JSON.stringify(dummyUserId));
+  //     onLogin(dummyUserId);
+  //   } else {
 
-      alert('회원가입이 완료되었습니다. 자동 로그인 처리합니다.');
-      setMode('login');
-      resetForm();
-      const dummyUserId = 'dummyUser';
-      localStorage.setItem('userId', JSON.stringify(dummyUserId));
-      onLogin(dummyUserId);
-    }
-  };
+  //     alert('회원가입이 완료되었습니다. 자동 로그인 처리합니다.');
+  //     setMode('login');
+  //     resetForm();
+  //     const dummyUserId = 'dummyUser';
+  //     localStorage.setItem('userId', JSON.stringify(dummyUserId));
+  //     onLogin(dummyUserId);
+  //   }
+  // };
 
   //여기 위까지 지우면 됩니다!
 
